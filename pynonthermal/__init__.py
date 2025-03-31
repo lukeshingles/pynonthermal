abc4a59b7a14d25ba2f19aa7e09c76c4e018390e
Luke Shingles <luke.shingles@gmail.com>
2025-03-31 12:19:45 +0100

    Fix pyright undefined attribute by exporting axelrod, collion, excitation

--- pynonthermal/__init__.py
+++ pynonthermal/__init__.py
@@ -1,3 +1,6 @@
+from pynonthermal import axelrod as axelrod
+from pynonthermal import collion as collion
+from pynonthermal import excitation as excitation
 from pynonthermal.base import DATADIR as DATADIR
 from pynonthermal.base import electronlossfunction as electronlossfunction
 from pynonthermal.base import get_energyindex_gteq as get_energyindex_gteq
