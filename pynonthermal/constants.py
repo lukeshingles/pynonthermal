0a5d2e461cf0b75e21d4d17d57669dc4ea4c495f
Luke Shingles <luke.shingles@gmail.com>
2025-04-02 13:35:07 +0100

    Update constants.py

--- pynonthermal/constants.py
+++ pynonthermal/constants.py
@@ -1,11 +1,8 @@
-import math
-
 # cgs units to match ARTIS
 EV = 1.6021772e-12  # eV in erg
-H = 6.6260755e-27  # in erg s
-ME = 9.1093897e-28  # mass of electron in g
+H = 6.6260755e-27  # Planck's constant [erg s]
+ME = 9.1093897e-28  # mass of electron [g]
 QE = 4.80325e-10  # elementary charge in statcoulomb
 H_ionpot = 13.5979996 * EV  # ionisation potential of hydrogen
-CLIGHT = 2.99792458e10  # speed of light in in cm/s
+CLIGHT = 2.99792458e10  # speed of light [cm/s]
 K_B = 8.617333262145e-5  # eV / K
-PI = math.pi
